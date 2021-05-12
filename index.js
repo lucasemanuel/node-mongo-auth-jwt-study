d7eefa3f6a837ee04c1d67c08fd7974e5a71d486
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-05-12 10:34:24 -0300

    adicionando conexão com mongo e setando rotas no app

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./src/routes')
+
+require('dotenv').config()
+
+const { DB_HOST, DB_PORT, DB_DATABASE } = process.env
+mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+mongoose.set('useCreateIndex', true)
+mongoose.connection.on('error', error => console.log(error))
+mongoose.Promise = global.Promise
 
 const app = express()
 const port = 3000
 
+app.use(express.json())
+app.use(routes)
 app.get('/', (req, res) => res.send('Hello World'))
+
 app.listen(port, () => console.log('Server start!'))
