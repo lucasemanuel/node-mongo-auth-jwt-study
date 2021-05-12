9f05caea918943f063ee374941a6b4470f1cdbd6
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-05-12 09:18:49 -0300

    criptografando senha antes de salvar usuário

--- src/model/user.js
+++ src/model/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
 
 const UserSchema = new Schema({
   email: {
@@ -13,4 +14,9 @@ const UserSchema = new Schema({
   }
 })
 
-module.exports = mongoose.Model('user', UserSchema)
+UserSchema.pre('save', async function (next) {
+  this.password = await bcrypt.hash(this.password, 10)
+  next()
+})
+
+module.exports = mongoose.model('user', UserSchema)
